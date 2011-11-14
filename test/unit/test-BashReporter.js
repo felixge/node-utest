cb48e0f449a065707ef2c06fcea33cf852b6cef4
Felix Geisendörfer <felix@debuggable.com>
2011-11-14 13:52:29 +0100

    Improved bash output
    As well as a test for the BashReporter

---
+++ test/unit/test-BashReporter.js
@@ -0,0 +1,32 @@
+var assert       = require('assert');
+var BashReporter = require('../../lib/reporter/BashReporter');
+var EventEmitter = require('events').EventEmitter;
+
+(function testOutputOfFailedTests() {
+  var process    = new EventEmitter();
+  var collection = new EventEmitter();
+  var reporter   = new BashReporter({process: process, collection: collection});
+
+  var stdout = '';
+  process.stdout = {
+    write: function(chunk) {
+      stdout += chunk;
+    },
+  }
+
+  var testCase = {name: 'MyTestCase'};
+  var test     = 'is awesome';
+  var error    = {stack: 'Something went wrong:\nLine2\nLine3'};
+
+  collection.emit('fail', testCase, test, error);
+
+  assert.equal(
+    stdout,
+    'Failed: MyTestCase is awesome\n' +
+    '\n' +
+    '  Something went wrong:\n' +
+    '  Line2\n' +
+    '  Line3\n' +
+    '\n'
+  );
+})();
