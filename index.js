26aeb7f52825b8fc301ffd013619780f3604346f
apla <dot.and.thing@gmail.com>
2012-01-22 00:37:37 +0400

    added support for async tasks + tasks depends on nextTick

--- index.js
+++ index.js
@@ -6,13 +6,21 @@ utest.BashReporter = require('./lib/reporter/BashReporter');
 var collection;
 var reporter;
 function utest(name, tests) {
+  var object, eventName;
+  if (arguments.length == 4 && tests.on && tests.on instanceof Function) {
+    object    = tests;
+	eventName = arguments[2];
+	tests     = arguments[3];
+  }
   if (!collection) {
     collection = new utest.Collection();
     reporter   = new utest.BashReporter({collection: collection});
 
-    process.nextTick(collection.run.bind(collection));
+    // process.nextTick(collection.run.bind(collection));
   }
 
   var testCase = new utest.TestCase({name: name, tests: tests});
-  collection.add(testCase);
+  collection.add(testCase, object, eventName);
+  
+  return testCase;
 };
